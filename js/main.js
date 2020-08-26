19bec8434995d561aaf1708eed398e39f2737943
jesda <jesda3@hotmail.com>
2020-08-26 14:25:05 -0700

    Agregandole plugins

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
     "use strict";
 
 
+
+
     document.addEventListener("DOMContentLoaded", function() {
 
         var map = L.map('mapa').setView([32.50636, -116.924068], 16);
@@ -147,5 +149,39 @@
     }); //DOM CONTENT LOADED
 
 
-
 })();
+
+$(function() {
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+    //Programa de Conferencias
+    $(".programa-evento .programa-info:first").show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $(".ocultar").hide();
+        var enlace = $(this).attr('href');
+        console.log(enlace)
+        $(enlace).fadeIn(1000);
+
+        return false;
+    });
+
+    //Animaciones para los Numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6 }, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15 }, 1400);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9 }, 1200);
+
+    //Cuenta Regresiva
+    $('.cuenta-regresiva').countdown('2020/10/30 9:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+});
